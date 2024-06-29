fb1217777ad1598aa851c3ec105d73cd452778f1
ArnashDhan <77660694+ArnashDhan@users.noreply.github.com>
2024-06-29 18:25:12 +0530

    Login is Working

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,43 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-<<<<<<< HEAD
-=======
+from os import path
+from flask_login import LoginManager
 
 db =SQLAlchemy()
 DB_NAME ="database.db"
->>>>>>> upstream/ADW
 
-db=SQLAlchemy
+db=SQLAlchemy()
 DB_NAME="database.db"
 def create_app():
     app = Flask(__name__)
-    app.config['secret_key'] = 'odoo_combat'
-<<<<<<< HEAD
-    app.config['SQLALHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
-=======
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://DB_NAME.sqlite3'
+    app.config['SECRET_KEY'] = 'This_is_a_Secret_Key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
 
     db.init_app(app)
 
->>>>>>> upstream/ADW
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views,url_prefix='/') 
     app.register_blueprint(auth,url_prefix='/') 
+
+    from .models import User
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app):
+    if not path.exists('website/'+DB_NAME):
+        db.create_all(app=app)
+        print ("created Database")
