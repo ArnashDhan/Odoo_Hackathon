888b9d9203e5bb8dce1eaff53af28447ae65b4e6
ArnashDhan <77660694+ArnashDhan@users.noreply.github.com>
2024-06-29 12:25:46 +0530

    The base Template of the website

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint , render_template
 
 auth=Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return "<p>login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/signup')
+@auth.route('/sign-up')
 def signup():
-    return "<p>signup</p>"
+    return render_template("signup.html")
 
 
